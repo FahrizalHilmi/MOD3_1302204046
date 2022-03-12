86c3332aa70266bb9151f912c968a6e0c1dd213e
rizal005 <fahrizalhilmi2002@gmail.com>
2022-03-12 16:29:12 +0700

    MENAMBAHKAN CLASS KODEBUAH

---
+++ MOD3_1302204046/MOD3_1302204046/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MOD3_1302204046
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            kodebuah nama = new kodebuah();
+            nama.getKodeBuah();
+        }
+    }
+}
